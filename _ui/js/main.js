af07ffd8169a081f9d9f48e6167bc66683f2bdd1
Gvn Suntop <gavin@gsuntop.com>
2012-12-27 15:22:32 -0800

    tabs -> spaces

--- _ui/js/main.js
+++ _ui/js/main.js
@@ -3,20 +3,20 @@ if (typeof window.ENO !== 'undefined') {
 }
 
 window.ENO = {
-	pool: {
-		notes: ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'],
-		tempo: [20, 200], // BPM
-		duration: [1, 100], // Bars
-		signature: [1, 8],
-		// topics: [],
-		// instruments: [],
-		tracks: [1, 16]
-	},
+    pool: {
+        notes: ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'],
+        tempo: [20, 200], // BPM
+        duration: [1, 100], // Bars
+        signature: [1, 8],
+        // topics: [],
+        // instruments: [],
+        tracks: [1, 16]
+    },
     init: function () {
         var self = this,
-        	item,
-        	$pool,
-        	$cast;
+            item,
+            $pool,
+            $cast;
 
         $cast = $('button');
 
@@ -27,46 +27,46 @@ window.ENO = {
         $pool = $('<dl/>');
 
         for (item in self.pool) {
-        	$pool.append('<dt>' + item + '</dt>');
-        	self.outputs[item] = $('<dd/>');
-        	$pool.append(self.outputs[item]);
+            $pool.append('<dt>' + item + '</dt>');
+            self.outputs[item] = $('<dd/>');
+            $pool.append(self.outputs[item]);
         }
 
         // Append Pool DL
         $('body').append($pool);
 
         $cast.click(function(event) {
-        	self.cast();
+            self.cast();
         });
     },
     cast: function () {
-    	var self = this,
-    		item,
-    		random,
-    		i,
-    		ii,
-    		textOutput;
+        var self = this,
+            item,
+            random,
+            i,
+            ii,
+            textOutput;
 
-    	for (item in self.pool) {
-    		if (typeof self.pool[item][0] === 'string') {
-    			textOutput = '';
+        for (item in self.pool) {
+            if (typeof self.pool[item][0] === 'string') {
+                textOutput = '';
 
-    			random = Math.floor(Math.random() * self.pool[item].length);
+                random = Math.floor(Math.random() * self.pool[item].length);
 
-    			for (i = 0, ii = random; i < ii; i++) {
-    				random = Math.floor(Math.random() * self.pool[item].length);
-    				textOutput += self.pool[item][random] + ' ';
-    			}
+                for (i = 0, ii = random; i < ii; i++) {
+                    random = Math.floor(Math.random() * self.pool[item].length);
+                    textOutput += self.pool[item][random] + ' ';
+                }
 
-    			self.outputs[item].text(textOutput);
-    		} else if (typeof self.pool[item][0] === 'number') {
-    			textOutput = '';
+                self.outputs[item].text(textOutput);
+            } else if (typeof self.pool[item][0] === 'number') {
+                textOutput = '';
 
-    			random = Math.floor(Math.random() * self.pool[item][1] + self.pool[item][0])
+                random = Math.floor(Math.random() * self.pool[item][1] + self.pool[item][0])
 
-    			self.outputs[item].text(random);
-    		}
-    	}
+                self.outputs[item].text(random);
+            }
+        }
     }
 };
 
